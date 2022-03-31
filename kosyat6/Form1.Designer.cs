020e9938f3e714400a8aeb5ffdb1411a8a156153
HellGoesOn <35145123+HellGoesOn@users.noreply.github.com>
2022-03-31 20:48:33 +0300

    Add project files.

---
+++ kosyat6/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿
+namespace kosyat6
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.vvod1 = new System.Windows.Forms.TextBox();
+            this.vvod2 = new System.Windows.Forms.TextBox();
+            this.vvod3 = new System.Windows.Forms.TextBox();
+            this.vvod4 = new System.Windows.Forms.TextBox();
+            this.vivod1 = new System.Windows.Forms.TextBox();
+            this.xx1 = new System.Windows.Forms.TextBox();
+            this.yy1 = new System.Windows.Forms.TextBox();
+            this.xx2 = new System.Windows.Forms.TextBox();
+            this.yy2 = new System.Windows.Forms.TextBox();
+            this.xx3 = new System.Windows.Forms.TextBox();
+            this.yy3 = new System.Windows.Forms.TextBox();
+            this.xx4 = new System.Windows.Forms.TextBox();
+            this.yy4 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 46);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "6.1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(402, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 226);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "7.1";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // vvod1
+            // 
+            this.vvod1.Location = new System.Drawing.Point(27, 23);
+            this.vvod1.Name = "vvod1";
+            this.vvod1.Size = new System.Drawing.Size(91, 23);
+            this.vvod1.TabIndex = 2;
+            this.vvod1.Text = "a";
+            // 
+            // vvod2
+            // 
+            this.vvod2.Location = new System.Drawing.Point(27, 52);
+            this.vvod2.Name = "vvod2";
+            this.vvod2.Size = new System.Drawing.Size(91, 23);
+            this.vvod2.TabIndex = 3;
+            this.vvod2.Text = "b";
+            // 
+            // vvod3
+            // 
+            this.vvod3.Location = new System.Drawing.Point(149, 23);
+            this.vvod3.Name = "vvod3";
+            this.vvod3.Size = new System.Drawing.Size(92, 23);
+            this.vvod3.TabIndex = 4;
+            this.vvod3.Text = "c";
+            // 
+            // vvod4
+            // 
+            this.vvod4.Location = new System.Drawing.Point(149, 52);
+            this.vvod4.Name = "vvod4";
+            this.vvod4.Size = new System.Drawing.Size(92, 23);
+            this.vvod4.TabIndex = 5;
+            this.vvod4.Text = "a";
+            // 
+            // vivod1
+            // 
+            this.vivod1.Location = new System.Drawing.Point(4, 133);
+            this.vivod1.Name = "vivod1";
+            this.vivod1.Size = new System.Drawing.Size(299, 23);
+            this.vivod1.TabIndex = 6;
+            // 
+            // textBox1
+            // 
+            this.xx1.Location = new System.Drawing.Point(333, 23);
+            this.xx1.Name = "textBox1";
+            this.xx1.Size = new System.Drawing.Size(53, 23);
+            this.xx1.TabIndex = 7;
+            // 
+            // textBox2
+            // 
+            this.yy1.Location = new System.Drawing.Point(333, 52);
+            this.yy1.Name = "textBox2";
+            this.yy1.Size = new System.Drawing.Size(53, 23);
+            this.yy1.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.xx2.Location = new System.Drawing.Point(333, 81);
+            this.xx2.Name = "textBox3";
+            this.xx2.Size = new System.Drawing.Size(53, 23);
+            this.xx2.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.yy2.Location = new System.Drawing.Point(333, 110);
+            this.yy2.Name = "textBox4";
+            this.yy2.Size = new System.Drawing.Size(53, 23);
+            this.yy2.TabIndex = 10;
+            // 
+            // textBox5
+            // 
+            this.xx3.Location = new System.Drawing.Point(334, 139);
+            this.xx3.Name = "textBox5";
+            this.xx3.Size = new System.Drawing.Size(52, 23);
+            this.xx3.TabIndex = 11;
+            // 
+            // textBox6
+            // 
+            this.yy3.Location = new System.Drawing.Point(334, 168);
+            this.yy3.Name = "textBox6";
+            this.yy3.Size = new System.Drawing.Size(52, 23);
+            this.yy3.TabIndex = 12;
+            // 
+            // textBox7
+            // 
+            this.xx4.Location = new System.Drawing.Point(334, 197);
+            this.xx4.Name = "textBox7";
+            this.xx4.Size = new System.Drawing.Size(52, 23);
+            this.xx4.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.yy4.Location = new System.Drawing.Point(334, 226);
+            this.yy4.Name = "textBox8";
+            this.yy4.Size = new System.Drawing.Size(52, 23);
+            this.yy4.TabIndex = 14;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(483, 126);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(100, 23);
+            this.textBox9.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.yy4);
+            this.Controls.Add(this.xx4);
+            this.Controls.Add(this.yy3);
+            this.Controls.Add(this.xx3);
+            this.Controls.Add(this.yy2);
+            this.Controls.Add(this.xx2);
+            this.Controls.Add(this.yy1);
+            this.Controls.Add(this.xx1);
+            this.Controls.Add(this.vivod1);
+            this.Controls.Add(this.vvod4);
+            this.Controls.Add(this.vvod3);
+            this.Controls.Add(this.vvod2);
+            this.Controls.Add(this.vvod1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox vvod1;
+        private System.Windows.Forms.TextBox vvod2;
+        private System.Windows.Forms.TextBox vvod3;
+        private System.Windows.Forms.TextBox vvod4;
+        private System.Windows.Forms.TextBox vivod1;
+        private System.Windows.Forms.TextBox xx1;
+        private System.Windows.Forms.TextBox yy1;
+        private System.Windows.Forms.TextBox xx2;
+        private System.Windows.Forms.TextBox yy2;
+        private System.Windows.Forms.TextBox xx3;
+        private System.Windows.Forms.TextBox yy3;
+        private System.Windows.Forms.TextBox xx4;
+        private System.Windows.Forms.TextBox yy4;
+        private System.Windows.Forms.TextBox textBox9;
+    }
+}
+
---
+++ kosyat6/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kosyat6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int Da(int x, int y)
+        {
+            return x * x + y * y; 
+        }
+
+        public float Rast(float x1, float y1, float x2, float y2)
+        {
+            return (float)Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        public float Geron(float a, float b, float c)
+        {
+            float p = (a + b + c) / 2;
+            return (float)Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            float x1, x2, x3, x4;
+            float y1, y2, y3, y;
+            x1 = Convert.ToSingle(xx1.Text);
+            y1 = Convert.ToSingle(yy1.Text);
+            x2 = Convert.ToSingle(xx2.Text);
+            y2 = Convert.ToSingle(yy2.Text);
+            x3 = Convert.ToSingle(xx3.Text);
+            y3 = Convert.ToSingle(yy3.Text);
+            x4 = Convert.ToSingle(xx4.Text);
+            x4 = Convert.ToSingle(yy4.Text);
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c, d;
+            a = Convert.ToInt32(vvod1.Text);
+            b = Convert.ToInt32(vvod2.Text);
+            c = Convert.ToInt32(vvod3.Text);
+            d = Convert.ToInt32(vvod4.Text);
+
+            if (Da(a, b) > Da(c, d))
+                vivod1.Text = "Пара A, B дала больший ответ";
+            else
+                vivod1.Text = "Пара C, D дала больший ответ";
+        }
+    }
+}
